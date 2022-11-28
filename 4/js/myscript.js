3b7d577835ae262bbe015574b09cd9eb4e596d23
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-28 18:46:53 +0100

    update

---
+++ 4/js/myscript.js
@@ -0,0 +1,44 @@
+/*
+Prendere due array della stessa lunghezza e generarne un terzo, prendendo alternativamente gli elementi da uno e dall'altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+ */
+
+const lista1 = ['a', 'b', 'c', 'd', 'e'];
+
+const lista2 = [1, 2, 3, 4, 5];
+
+let lista3 = [];
+
+let i = 0;
+
+while (lista3.length !== (lista1.length * 2)){
+    lista3.push(lista1[i]);
+    lista3.push(lista2[i]);
+    i = i+1;
+}
+
+console.log(lista3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
