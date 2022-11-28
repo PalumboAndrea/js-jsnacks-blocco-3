09f6a528fb33e14cdb77e28789dd9cd688de5cc8
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-28 16:37:47 +0100

    update

---
+++ 1/js/myscript.js
@@ -0,0 +1,37 @@
+/*
+Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+Math.floor((Math.random() * 9)+1)
+ */
+
+let number = 10;
+
+let requestedNumber = parseInt(prompt('Inserire un numero'));
+
+
+while (requestedNumber !== number){
+    requestedNumber = parseInt(prompt('Inserire un numero'));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
