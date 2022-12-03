ea8db6791eb4b31a4a7d06628040e10046f3bce5
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-03 18:16:24 +0100

    update

--- 9/js/myscript.js
+++ 9/js/myscript.js
@@ -6,6 +6,7 @@ i numeri che ha visto precedentemente tramite prompt.
 dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati indovinati
  */
 
+
 let container = document.querySelector('.container');
 
 const startEasy = document.getElementById('start-easy');
@@ -20,8 +21,17 @@ let randomNumber = document.createElement('div');
 
 let userNumber;
 
-function getRandomNumber (min, max){
+let level;
+
+let computerNumberChoice = [];
+
+let userNumberChoice = [];
+
+let winOrNot = document.getElementById('win-or-not');
+
+function getRandomNumber (min, max, array){
     const random = Math.floor(Math.random() * (max - min +1) + min);
+    array.push(random);
     return random;
 }
 
@@ -29,33 +39,72 @@ function difficultyLevel (level, whereToPut){
     for (i=0; i<level; i++){
         randomNumber.classList.add('my-4')
         whereToPut.append(randomNumber);
-        randomNumber.innerHTML += (getRandomNumber(1, 100)) + ' ';
+        randomNumber.innerHTML += (getRandomNumber(1, 100, computerNumberChoice)) + ' ';
     }
 }
 
-function disappear (a){
+function disappear (){
     randomNumber.innerHTML = '';
-    let a = prompt('Inserisci i numeri che hai visto in ordine');
-    return a;
+}
+
+function askNumber (level, array1, array2){
+
+    for (i=0; i<level; i++){
+        const userNumber = parseInt(prompt('Inserire'), 10);
+        array1.push(userNumber);
+    }
+    
+    let win;
+    for (i=0; i<level; i++){
+        if (array1[i] == array2[i]){
+            win = 1;
+        } else {
+            win = 0;
+        }   
+    }
+    
+    if (win == 1){
+        let youWon = document.createElement('h1');
+        winOrNot.append(youWon);
+        youWon.innerHTML = 'HAI VINTO!';
+    } else {
+        let youWon = document.createElement('h1');
+        winOrNot.append(youWon);
+        youWon.innerHTML = 'HAI PERSO!';
+    }
+
+    randomNumber.innerHTML = 'I tuoi numeri: ' + userNumberChoice + ' I numeri del computer: ' + computerNumberChoice;
+
 }
 
 startEasy.addEventListener('click', function (){
+    numberContainerEasy.innerHTML = '';
+    winOrNot.innerHTML = '';
+    level = 5;
     disappear();
-    difficultyLevel(5, numberContainerEasy);
-    setTimeout (disappear, 5000, userNumber);
-
+    difficultyLevel(level, numberContainerEasy);
+    setTimeout (disappear, 5000);
+    setTimeout (askNumber, 5100, level, userNumberChoice, computerNumberChoice);
 })
 
 startMiddle.addEventListener('click', function (){
+    numberContainerEasy.innerHTML = '';
+    winOrNot.innerHTML = '';
+    level = 10;
     disappear();
-    difficultyLevel(10, numberContainerMiddle);
+    difficultyLevel(level, numberContainerMiddle);
     setTimeout (disappear, 5000);
+    setTimeout (askNumber, 5100, level, userNumberChoice, computerNumberChoice);
 })
 
 startHard.addEventListener('click', function (){
+    numberContainerEasy.innerHTML = '';
+    winOrNot.innerHTML = '';
+    level = 15;
     disappear();
-    difficultyLevel(15, numberContainerHard);
+    difficultyLevel(level, numberContainerHard);
     setTimeout (disappear, 5000);
+    setTimeout (askNumber, 5100, level, userNumberChoice, computerNumberChoice);
 })
 
 
